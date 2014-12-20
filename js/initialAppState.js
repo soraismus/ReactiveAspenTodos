184e5c8fcbe6402420ed0c135e0ab94a558e2201
Soraismus <tertius_lupus@yahoo.com>
2014-12-19 19:30:15 -0500

    Recompile and rebrowserify.

--- js/initialAppState.js
+++ js/initialAppState.js
@@ -1,33 +1,39 @@
-var activeCount, count, mode, todos;
+var activeCount, count, focus, mode, todos;
+
+activeCount = 2;
+
+count = 3;
+
+focus = true;
+
+mode = 'all';
 
 todos = [
   {
     completed: false,
     editing: false,
     editText: null,
+    focus: false,
     title: 'think'
   }, {
     completed: true,
     editing: false,
     editText: null,
+    focus: false,
     title: 'ponder'
   }, {
     completed: false,
     editing: false,
     editText: null,
+    focus: false,
     title: 'reflect'
   }
 ];
 
-activeCount = 2;
-
-count = 3;
-
-mode = 'all';
-
 module.exports = {
   activeCount: activeCount,
   count: count,
+  focus: focus,
   mode: mode,
   todos: todos
 };
--- js/output.js
+++ js/output.js
@@ -315,36 +315,42 @@ todoItemInput = 'todo-item-input';
 module.exports = [['$todo-item-input-events', 'TodoItemInput'], ['$toggle-all-clicks', 'toggle-all-checkbox'], ['$toggle-clicks', 'completion-toggle'], ['$destroy-clicks', 'destroy-button'], ['$clear-clicks', 'ClearButton'], ['$todo-label-doubleclicks', 'todo-item-label'], ['$new-todo-keydowns', 'new-todo-input'], ['$edit-blurs', todoItemInput, onBlur], ['todo-in-edit', todoItemInput, onChange], ['$edit-keydowns', todoItemInput, onKeyDown], ['$active-todos-clicks', 'ActiveTodos'], ['$all-todos-clicks', 'AllTodos'], ['$completed-todos-clicks', 'CompletedTodos']];
 
 },{}],3:[function(require,module,exports){
-var activeCount, count, mode, todos;
+var activeCount, count, focus, mode, todos;
+
+activeCount = 2;
+
+count = 3;
+
+focus = true;
+
+mode = 'all';
 
 todos = [
   {
     completed: false,
     editing: false,
     editText: null,
+    focus: false,
     title: 'think'
   }, {
     completed: true,
     editing: false,
     editText: null,
+    focus: false,
     title: 'ponder'
   }, {
     completed: false,
     editing: false,
     editText: null,
+    focus: false,
     title: 'reflect'
   }
 ];
 
-activeCount = 2;
-
-count = 3;
-
-mode = 'all';
-
 module.exports = {
   activeCount: activeCount,
   count: count,
+  focus: focus,
   mode: mode,
   todos: todos
 };
